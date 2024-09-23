ed4cb5754b0c3526af1c94a7caadebe58899a6b8
Dhylan Patel <dp521@ic.ac.uk>
2024-09-23 21:15:56 +0100

    gptfix2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById("dem").addEventListener("click", tally1);
+document.getElementById("dem").addEventListener("click", tallydem);
 let demtally = 0
 function tally1(){
     demtally = demtally + 1
     document.getElementById("demtally").innerHTML = demtally
 }
 
-document.getElementById("rep").addEventListener("click", tally1);
+document.getElementById("rep").addEventListener("click", tallyrep);
 let reptally = 0
 function tally1(){
     reptally = reptally + 1
