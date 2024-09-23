161d4c70acc748f6760b7d4f051d223de45d8437
Dhylan Patel <dp521@ic.ac.uk>
2024-09-23 21:23:34 +0100

    fix1

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById("dem").addEventListener("click", tallydem);
 let demtally = 0;
-function tally1(){
+function tallydem(){
     demtally +=1
     document.getElementById("demtally").innerHTML = demtally
 }
 
 document.getElementById("rep").addEventListener("click", tallyrep);
 let reptally = 0;
-function tally1(){
+function tallyrep(){
     reptally +=1;
     document.getElementById("reptally").innerHTML = reptally
 }
